7479dd5e3a63268fb484a1a0301bd949751a178e
Igor Elias <igor.elias.muniz@gmail.com>
2017-09-15 16:31:16 -0300

    config express

---
+++ src/config/server.js
@@ -0,0 +1,11 @@
+const PORT = 3030
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
