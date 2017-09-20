96718690f69b82e2c3688e24900a109b73b32281
Igor Elias <igor.elias.muniz@gmail.com>
2017-09-20 15:55:37 -0300

    add cors

--- src/config/server.js
+++ src/config/server.js
@@ -2,9 +2,11 @@ const PORT = 3030
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const cors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
